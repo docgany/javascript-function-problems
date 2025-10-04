280655276073036927bbf3f20b6567a240623035
Grace Adaeze Okengwu <136967329+docgany@users.noreply.github.com>
2025-10-04 22:10:09 +0100

    Initial commit to commence my array-problems.

---
+++ array-problems/array-problem1/script.js
@@ -0,0 +1,8 @@
+/*Write a JavaScript function that takes in two parameters, an array and a number(n), and return the first n elements of that array.*/
+
+const arr = [2, 5, 7, 9, 10, 4, 3];
+const n = 3;
+
+function getArrCopy(arr, n) {
+    
+}
