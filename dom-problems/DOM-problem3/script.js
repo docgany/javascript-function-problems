b42e63efece4a58162c1c480217406c6273cd505
Grace Adaeze Okengwu <136967329+docgany@users.noreply.github.com>
2025-10-04 10:12:45 +0100

    Last DOM Project done and dusted. taking my mentor's advice of being focused very seriously.

--- dom-problems/DOM-problem3/script.js
+++ dom-problems/DOM-problem3/script.js
@@ -1,25 +1,19 @@
-/*Create an HTML page with a dropdown list with three items in it, then write a JavaScript function that displays the number of items and displays the selected item in the list when you select from the dropdown.*/
+/*Create an HTML page with a dropdown list with three items in it, then write a JavaScript function that displays the number of items and displays the item in the list when you select from the dropdown.
+STEP1: Add EventListeners; onchange to the select element and button repectively. And thisis after accessing and storing this elemnents in a variable for easy usage.
+Stage2: define the function that displays the lenghth of the drop-down items and also outputs the values of the items as strings by looping. through the arrray.
+Stage3: Using the 'change'event' event handler triggered when we select another drop-down item, we run a function which we will define next now*/
 
-const selectEl = document.getElementById("drpdown-list");
-let countEl = document.getElementById("item-count");
-let listValues = document.getElementById("list-values");
-let result = document.getElementById("ouput");
-let i = selectEl.selectedIndex;
-result.textContent = selectEl.options[i].text;
-countEl.textContent = selectEl.options.length;
+const selectEl = document.getElementById("mySelect");
 
-let txt = "";
-for (let i = 0; i < selectEl.length; i++) {
-    txt = txt + selectEl.options[i].text + "  ,";
-    return txt;
-}
+selectEl.addEventListener('change', () => {
+    let y = selectEl.value;
+    document.getElementById("selectedItem").textContent = `You selected : ${y}`;
 
-console.log(txt);
-
-
-
-/*selectEl.addEventListener('onchange', (e) => {
-    e.preventDefault;
-   
-    
-})*/
+    let txt = "";
+    for (let i = 0; i < selectEl.length; i++) {
+        txt += selectEl.options[i].text + " , ";
+    }
+    document.getElementById('items').textContent = `The items in the drop-down list are as follows:
+     ${txt}`;
+    document.getElementById("item-length").textContent = `There are ${selectEl.options.length} items in the drop-down list.`;
+})                                                                                                                                                                        
